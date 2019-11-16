8dc2de3ceadc9c421b35a4970afe29dc2d64df2b
doct0rX <mustafaj4m@gmail.com>
2019-11-16 16:27:20 +0200

    upding - styles.xml - andding splash screen - needs to fix splash screen to be displayed and redirect to LoginActivity

---
+++ app/src/main/java/com/doct0rx/sparrow/ui/splash/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.doct0rx.sparrow.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.doct0rx.sparrow.R;
+import com.doct0rx.sparrow.ui.login.LoginActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;    // 3 seconds
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goToMainActivity();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+    private void goToMainActivity() {
+        Intent mainActivityIntent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(mainActivityIntent);
+        finish();
+    }
+}
