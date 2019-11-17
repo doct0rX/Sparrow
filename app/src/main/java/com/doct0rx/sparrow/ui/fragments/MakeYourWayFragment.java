6fc6a0f01ba34f7dc84ffa7193b97cde9670e95f
doct0rX <mustafaj4m@gmail.com>
2019-11-17 07:45:42 +0200

    adding MapActivity and linking it with MakeYourWayFragment buttons

--- app/src/main/java/com/doct0rx/sparrow/ui/fragments/MakeYourWayFragment.java
+++ app/src/main/java/com/doct0rx/sparrow/ui/fragments/MakeYourWayFragment.java
@@ -1,17 +1,25 @@
 package com.doct0rx.sparrow.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.doct0rx.sparrow.R;
+import com.doct0rx.sparrow.ui.fragments.maps.MapsActivity;
 
 public class MakeYourWayFragment extends Fragment {
 
     private View view;
+    private Button travelFasterButton;
+    private Button travelGreenerButton;
+    private Button travelAroundButton;
+    private Button visitWonderlandButton;
+
     public MakeYourWayFragment() {
         // Required empty public constructor
     }
@@ -21,7 +29,42 @@ public class MakeYourWayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_make_your_way, container, false);
+        travelFasterButton = view.findViewById(R.id.travelFasterButton);
+        travelFasterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startMapActivity = new Intent(getActivity(), MapsActivity.class);
+                startActivity(startMapActivity);
+            }
+        });
 
+        travelGreenerButton = view.findViewById(R.id.travelGreenerButton);
+        travelGreenerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startMapActivity = new Intent(getActivity(), MapsActivity.class);
+                startActivity(startMapActivity);
+            }
+        });
+
+        travelAroundButton = view.findViewById(R.id.travelAroundButton);
+        travelAroundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startMapActivity = new Intent(getActivity(), MapsActivity.class);
+                startActivity(startMapActivity);
+            }
+        });
+
+        visitWonderlandButton = view.findViewById(R.id.visitAWonderlandButton);
+        visitWonderlandButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startMapActivity = new Intent(getActivity(), MapsActivity.class);
+                startActivity(startMapActivity);
+            }
+        });
+        
         return view;
     }
 }
